46c75b506ef01a737035eeb835f555c8cdfc26ea
HuGanghui <huganghui18@gmail.com>
2020-07-01 18:39:13 +0800

    Issue #63: Distinguish between new and changed lines

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionPatchFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionPatchFilter.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -61,6 +62,11 @@ public class SuppressionPatchFilter extends AutomaticBean
     private boolean optional;
 
     /**
+     * Control if only consider added lines in file.
+     */
+    private String strategy = "all";
+
+    /**
      * Set of individual suppresses.
      */
     private PatchFilterSet filters = new PatchFilterSet();
@@ -84,6 +90,15 @@ public class SuppressionPatchFilter extends AutomaticBean
      */
     public void setOptional(boolean optional) {
         this.optional = optional;
+    }
+
+    /**
+     * Setter to control if only consider added lines in file.
+     *
+     * @param strategy tells if only consider added lines is add, should be added or changed.
+     */
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
     }
 
     @Override
@@ -159,14 +174,32 @@ public class SuppressionPatchFilter extends AutomaticBean
 
     private List<List<Integer>> getLineRange(Patch<String> patch) {
         final List<List<Integer>> lineRangeList = new ArrayList<>();
+        final TotalNumber totalNumber = new TotalNumber();
         for (int i = 0; i < patch.getDeltas().size(); i++) {
-            final Chunk targetChunk = patch.getDeltas().get(i).getTarget();
-            final List<Integer> lineRange = new ArrayList<>();
-            lineRange.add(targetChunk.getPosition());
-            lineRange.add(targetChunk.getPosition() + targetChunk.getLines().size());
-            lineRangeList.add(lineRange);
+            final List<Integer> removePositionList = getRemovePositionList(patch, i);
+            final List<Integer> addPositionList = getAddPositionList(patch, i);
+            final int addPositionListSize = addPositionList.size();
+            if ("added".equals(strategy) && addPositionListSize != 0) {
+                final AddLineRange addLineRange =
+                        new AddLineRange(removePositionList, addPositionList, totalNumber);
+                addLineRange.getAddedLineRange();
+                lineRangeList.add(addLineRange.newPositionList);
+            }
+            else {
+                lineRangeList.add(addPositionList);
+            }
         }
         return lineRangeList;
+    }
+
+    private List<Integer> getRemovePositionList(Patch<String> patch, int index) {
+        final Chunk originChunk = patch.getDeltas().get(index).getSource();
+        return originChunk.getChangePosition();
+    }
+
+    private List<Integer> getAddPositionList(Patch<String> patch, int index) {
+        final Chunk targetChunk = patch.getDeltas().get(index).getTarget();
+        return targetChunk.getChangePosition();
     }
 
     @Override
@@ -174,4 +207,276 @@ public class SuppressionPatchFilter extends AutomaticBean
         return Collections.singleton(file);
     }
 
+    /**
+     * To get real added lines' positions.
+     */
+    private class AddLineRange {
+
+        /**
+         * It contains current Delta's the real added lines' positions.
+         */
+        private final List<Integer> newPositionList = new ArrayList<>();
+
+        /**
+         * It contains source and target files' offset numbers.
+         */
+        private final TotalNumber totalNumber;
+
+        /**
+         * It represents current Delta's removePositionList.
+         */
+        private final List<Integer> removePositionList;
+
+        /**
+         * It represents current Delta's addPositionList.
+         */
+        private final List<Integer> addPositionList;
+
+        /**
+         * It represents size of removePositionList.
+         */
+        private final int removePositionListSize;
+
+        /**
+         * It represents size of addPositionList.
+         */
+        private final int addPositionListSize;
+
+        /**
+         * It represents current index in removePositionList.
+         */
+        private int removeIndex;
+
+        /**
+         * It represents current index in addPositionList.
+         */
+        private int addIndex;
+
+        /**
+         * It decides which position list to process,
+         * true is addPositionList,
+         * false is removePositionList.
+         */
+        private boolean changeFlag;
+
+        AddLineRange(List<Integer> removePositionList,
+                            List<Integer> addPositionList, TotalNumber totalNumber) {
+            this.totalNumber = totalNumber;
+            this.removePositionList = removePositionList;
+            this.addPositionList = addPositionList;
+            this.removePositionListSize = removePositionList.size();
+            this.addPositionListSize = addPositionList.size();
+        }
+
+        private void initChangeFlag() {
+            if (removePositionListSize == 0) {
+                changeFlag = true;
+            }
+            else {
+                changeFlag = addPositionList.get(addIndex)
+                        < removePositionList.get(removeIndex);
+            }
+        }
+
+        public void getAddedLineRange() {
+            initChangeFlag();
+            int totalAddNum = totalNumber.getTotalAddNum();
+            int totalRemoveNum = totalNumber.getTotalRemoveNum();
+            while (addIndex < addPositionListSize) {
+                if (changeFlag || removeIndex >= removePositionListSize) {
+                    final List<Integer> updateTotalNumList =
+                            processAddPositionList(totalAddNum, totalRemoveNum);
+                    totalAddNum = updateTotalNumList.get(0);
+                    totalRemoveNum = updateTotalNumList.get(1);
+                }
+                else {
+                    final List<Integer> updateTotalNumList =
+                            processRemovePositionList(totalAddNum, totalRemoveNum);
+                    totalAddNum = updateTotalNumList.get(0);
+                    totalRemoveNum = updateTotalNumList.get(1);
+                }
+            }
+            totalNumber.setTotalAddNum(totalAddNum);
+            totalNumber.setTotalRemoveNum(totalRemoveNum);
+        }
+
+        private List<Integer> processAddPositionList(int totalAddNum, int totalRemoveNum) {
+            int tempTotalAddNum = totalAddNum;
+            int tempTotalRemoveNum = totalRemoveNum;
+            // change flag to process remove position list
+            changeFlag = !changeFlag;
+            final int position = addPositionList.get(addIndex);
+            final int matchingRemovePositon = position - tempTotalAddNum
+                    + tempTotalRemoveNum;
+            // Judge if there are matching line in the remove position list
+            // if not, then it is added line
+            if (!removePositionList.contains(matchingRemovePositon)) {
+                newPositionList.add(position);
+                tempTotalAddNum++;
+                final List<Integer> increaseAddList =
+                        dealContinuousPosition(addIndex, addPositionList,
+                                true, newPositionList);
+                tempTotalAddNum += increaseAddList.get(0);
+                addIndex += increaseAddList.get(1);
+            }
+            else {
+                final int matchingRemovePositionIndex =
+                        removePositionList.indexOf(matchingRemovePositon);
+                final boolean flag =
+                        matchingRemovePositionIndex
+                                >= removeIndex;
+                if (flag) {
+                    removeIndex++;
+                }
+                final List<Integer> increaseRemoveList =
+                        dealContinuousPosition(matchingRemovePositionIndex,
+                                removePositionList, flag);
+                tempTotalRemoveNum += increaseRemoveList.get(0);
+                removeIndex += increaseRemoveList.get(1);
+                final List<Integer> increaseAddList =
+                        dealContinuousPosition(addIndex, addPositionList, true);
+                tempTotalAddNum += increaseAddList.get(0);
+                addIndex += increaseAddList.get(1);
+            }
+            addIndex++;
+            return Arrays.asList(tempTotalAddNum, tempTotalRemoveNum);
+        }
+
+        private List<Integer> processRemovePositionList(int totalAddNum, int totalRemoveNum) {
+            final int position = removePositionList.get(removeIndex);
+            int tempTotalRemoveNum = totalRemoveNum;
+            final int matchingAddPositon = position + totalAddNum - tempTotalRemoveNum;
+            // Judge if there are matching line in the add position list
+            // if not, then it is just removed line
+            if (!addPositionList.contains(matchingAddPositon)) {
+                tempTotalRemoveNum++;
+                final int increaseRemoveNum = dealContinuousPosition(addIndex,
+                        removePositionList).get(0);
+                tempTotalRemoveNum += increaseRemoveNum;
+            }
+            else {
+                final int increaseRemoveIndex = dealContinuousPosition(removeIndex,
+                        removePositionList).get(0);
+                removeIndex += increaseRemoveIndex;
+            }
+            removeIndex++;
+            // Judge if change flag to deal with add position list
+            // this judgment is for a certain situation that two or more
+            // removed fragment are continuous, this situation can be found
+            // in testAddOptionThree
+            if (removeIndex >= removePositionListSize) {
+                changeFlag = !changeFlag;
+            }
+            else if (removePositionList.get(removeIndex)
+                    >= addPositionList.get(addIndex)) {
+                changeFlag = !changeFlag;
+            }
+            else if (removePositionList.get(removeIndex)
+                    < addPositionList.get(addIndex)) {
+                // test if previous removeIndex is a removed fragment
+                final int testposition = removePositionList.get(removeIndex);
+                final int testmatchingAddPositon = testposition
+                        + totalAddNum - tempTotalRemoveNum
+                        + dealAddPosition(totalAddNum, tempTotalRemoveNum);
+                if (addPositionList.contains(testmatchingAddPositon)) {
+                    changeFlag = !changeFlag;
+                }
+            }
+            return Arrays.asList(totalAddNum, tempTotalRemoveNum);
+        }
+
+        private List<Integer> dealContinuousPosition(int startIndex,
+                                                     List<Integer> positionList) {
+            return dealContinuousPosition(startIndex, positionList, false);
+        }
+
+        private List<Integer> dealContinuousPosition(int startIndex,
+                                                     List<Integer> positionList,
+                                                     boolean updateIndex) {
+            return dealContinuousPosition(startIndex, positionList, updateIndex, null);
+        }
+
+        private List<Integer> dealContinuousPosition(int startIndex,
+                                                     List<Integer> positionList,
+                                                     boolean updateIndex,
+                                                     List<Integer> newAddPositionList) {
+            int tempNum = 0;
+            int tempIndex = 0;
+            for (int k = startIndex + 1; k < positionList.size(); k++) {
+                final int tempPosition = positionList.get(k - 1);
+                final int nextAddPosition = positionList.get(k);
+                if (nextAddPosition - tempPosition == 1) {
+                    tempNum++;
+                    if (updateIndex) {
+                        tempIndex++;
+                    }
+                    if (newAddPositionList != null) {
+                        newAddPositionList.add(nextAddPosition);
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+            return Arrays.asList(tempNum, tempIndex);
+        }
+
+        private int dealAddPosition(int totalAddNum, int totalRemoveNum) {
+            final int position = addPositionList.get(addIndex);
+            int tempAddNum = 0;
+            int tempRemoveNum = 0;
+            final int matchingRemovePositon = position - totalAddNum + totalRemoveNum;
+            if (!removePositionList.contains(matchingRemovePositon)) {
+                tempAddNum++;
+                final int increaseAddNum = dealContinuousPosition(addIndex, addPositionList).get(0);
+                tempAddNum += increaseAddNum;
+            }
+            else {
+                final int increaseRemoveNum = dealContinuousPosition(
+                        removePositionList.indexOf(matchingRemovePositon),
+                        removePositionList).get(0);
+                tempRemoveNum += increaseRemoveNum;
+            }
+            final int increaseAddNum = dealContinuousPosition(addIndex, addPositionList).get(0);
+            tempAddNum += increaseAddNum;
+
+            int diffNum = tempAddNum - tempRemoveNum;
+
+            if (matchingRemovePositon >= removePositionList.get(removeIndex)) {
+                diffNum = 0;
+            }
+            return diffNum;
+        }
+    }
+
+    /**
+     * TotalNumber Bean.
+     */
+    private class TotalNumber {
+        /**
+         * Represents the total offset num in target file.
+         */
+        private int totalAddNum;
+
+        /**
+         * Represents the total offset num in source file.
+         */
+        private int totalRemoveNum;
+
+        public int getTotalAddNum() {
+            return totalAddNum;
+        }
+
+        public void setTotalAddNum(int totalAddNum) {
+            this.totalAddNum = totalAddNum;
+        }
+
+        public int getTotalRemoveNum() {
+            return totalRemoveNum;
+        }
+
+        public void setTotalRemoveNum(int totalRemoveNum) {
+            this.totalRemoveNum = totalRemoveNum;
+        }
+    }
 }
