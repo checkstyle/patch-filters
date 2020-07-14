24ea74ea71c660d3b7c02dca7e26ea8c207e7c8e
HuGanghui <huganghui18@gmail.com>
2020-07-14 19:07:34 +0800

    Issue #74: Create unit tests for line strategy for Checker checks (non-TreeWalker)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/strategy/newline/FileTabCharacter/Test.java
@@ -0,0 +1,26 @@
+package Checker.FileTabCharacter;
+
+public class Test {
+    // Long line ----------------------------------------------------------------
+    // Contains a tab ->	<- //warn
+    // Contains trailing whitespace ->
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Long line ----------------------------------------------------------------
+    // Long line ----------------------------------------------------------------
+    // Contains a tab ->	<- //warns
+    // Contains trailing whitespace ->
+
+
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/strategy/newline/LineLength/LineLength.java
@@ -0,0 +1,12 @@
+package Checker;
+
+public class LineLength {
+    public static void main(String[] args) {
+        System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+    }
+}
