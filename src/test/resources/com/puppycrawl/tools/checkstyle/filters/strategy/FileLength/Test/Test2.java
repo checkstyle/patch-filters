4c0aa4d18f1e63505cbc5f14f96bc94b9e7622dd
HuGanghui <huganghui18@gmail.com>
2020-07-14 19:07:34 +0800

    Issue #74: Create unit tests for line strategy for Checker checks (non-TreeWalker)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/strategy/FileLength/Test/Test2.java
@@ -0,0 +1,14 @@
+package Checker.FileLength;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/strategy/RegexpMultiline/Test.java
@@ -0,0 +1,34 @@
+package Checker.RegexpMultiline;
+
+public class Test {
+    void method() {
+        System.out.
+                print("Example"); // line3
+
+        System.out.
+                print("Example"); // line3
+
+        System.out.
+                print("Example"); // line3
+
+
+
+
+
+        System.out.
+                print("Example");
+
+
+
+
+
+
+
+        ////
+        System.out.
+                print("Example");
+
+
+
+    }
+}
