67800d05dd7b30294484a896deea71e4f9cf6d4b
HuGanghui <huganghui18@gmail.com>
2020-07-16 16:47:15 +0800

    Issue #92: Add Non-Treewalker filter tests to cover more cases(FileTabCharacter)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/strategy/FileTabCharacter/SingleNewLine/Test.java
@@ -0,0 +1,25 @@
+package Checker.FileTabCharacter;
+
+public class Test {
+    // Long line ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Long line ----------------------------------------------------------------
+    // Contains a tab ->	<- //warn
+    // Contains a tab ->	<- //warn  // violation newline/patchedline
+    // Long line ----------------------------------------------------------------
+    // Contains a tab ->	<- //warns
+
+
+}
