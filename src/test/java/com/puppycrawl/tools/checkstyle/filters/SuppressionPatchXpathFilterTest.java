ec654d6b249e03c5e8eab96550531049bac8f8d9
HuGanghui <huganghui18@gmail.com>
2020-07-18 12:51:41 +0800

    Issue #101: use system variable to keep common part of path for tests

--- src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressionPatchXpathFilterTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressionPatchXpathFilterTest.java
@@ -77,6 +77,7 @@ public class SuppressionPatchXpathFilterTest extends AbstractModuleTestSupport {
     private void testByConfigTreeWalker(String configPath, String inputFile, String[] expected)
             throws Exception {
         // we can add here any variable to provide path to patch name by PropertiesExpander
+        System.setProperty("tp", "src/test/resources/com/puppycrawl/tools/checkstyle/filters");
         final Configuration config = ConfigurationLoader.loadConfiguration(
                 getPath(configPath),
                 new PropertiesExpander(System.getProperties()));
