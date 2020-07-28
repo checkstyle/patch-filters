6854f071c145a989df07a442d387601e8fc8f21f
HuGanghui <huganghui18@gmail.com>
2020-07-28 16:42:54 +0800

    Issue #164: SuppressionPatchXpathFilter: HiddenField's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/HiddenField/patchedline/Test.java
@@ -0,0 +1,20 @@
+package TreeWalker.coding.HiddenField;
+
+public class Test {
+    private int test;
+
+    private void addTest(int test)  // violation without filter
+    {
+        System.out.println(test);
+    }
+
+    private void addTest1(int test)  // violation without filter
+    {
+        System.out.println(test);
+    }
+
+    private void foo()
+    {
+        final int test = 1;  // violation without filter
+    }
+}
