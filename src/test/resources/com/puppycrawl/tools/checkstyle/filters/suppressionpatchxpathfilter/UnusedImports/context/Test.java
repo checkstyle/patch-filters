e2c7e365ed42fc949ee8c81c6fcbd078fba751ea
HuGanghui <huganghui18@gmail.com>
2020-07-31 22:19:49 +0800

    Issue #214: SuppressionPatchXpathFilter: UnusedImports's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/UnusedImports/context/Test.java
@@ -0,0 +1,8 @@
+package TreeWalker.UnusedImports;
+
+import java.io.File;  // violation without filter
+
+public class Test {
+    public void test(String input) {
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/UnusedImports/patchedline/Test.java
@@ -0,0 +1,9 @@
+package TreeWalker.UnusedImports;
+
+import java.io.File;  // violation without filter
+import java.io.FileInputStream;  // violation without filter
+
+public class Test {
+    public void test(String input) {
+    }
+}
