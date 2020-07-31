2e60bc4b74f88c5b06ad82b8ca97e554b20e7a98
HuGanghui <huganghui18@gmail.com>
2020-07-31 20:08:37 +0800

    Issue #204: SuppressionPatchXpathFilter: EmptyLineSeparator's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/EmptyLineSeparator/patchedline/Test.java
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////
+//HEADER
+///////////////////////////////////////////////////
+package TreeWalker.EmptyLineSeparator;  // violation without filter
+
+public class Test {
+    public static final int FOO_CONST = 1;
+    public void foo() {}  // violation without filter
+    public void foo2() {}  // violation without filter
+}
