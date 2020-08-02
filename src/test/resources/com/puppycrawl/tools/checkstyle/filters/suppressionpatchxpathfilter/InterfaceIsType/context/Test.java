96a07393384d4dfdb841d9cba4646065bb1bc48e
HuGanghui <huganghui18@gmail.com>
2020-08-02 17:00:15 +0800

    Issue #220: modify comment and add newline example about InterfaceIsType

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/InterfaceIsType/context/Test.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/InterfaceIsType/context/Test.java
@@ -3,15 +3,15 @@ package TreeWalker.InterfaceIsType;
 public class Test {
 }
 
-interface Test1 {  // violation context
+interface Test1 {  // violation
     int a = 32;
 }
 
-interface Test2 { // violation context
+interface Test2 { // violation
 
     int a = 32;
 }
 
-interface Test3 { // violation context
+interface Test3 { // violation
     int a = 3;
 }
