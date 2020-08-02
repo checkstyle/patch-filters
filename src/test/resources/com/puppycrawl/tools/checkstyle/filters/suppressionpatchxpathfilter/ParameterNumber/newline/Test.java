88e9074a1a7be1c741396fed48a63188d8930e08
HuGanghui <huganghui18@gmail.com>
2020-08-02 16:47:08 +0800

    Issue #220: modify comment and add newline example about ParameterNumber

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/ParameterNumber/newline/Test.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/ParameterNumber/newline/Test.java
@@ -1,8 +1,15 @@
-package TreeWalker.ParameterNumber;
+package TreeWalker.size.ParameterNumber;
 
 public class Test {
-    public void test(int aArg1, int aArg2, int aArg3, int aArg4, int aArg5,  // violation without filter
+    public void test0(int aArg1, int aArg2, int aArg3, int aArg4, int aArg5,  // violation without filter
                      int aArg6, int aArg7, int aArg8, int aArg9, int aArg10) {
+
+    }
+}
+
+class Test1 {
+    public void test0(int aArg1, int aArg2, int aArg3, int aArg4, int aArg5,  // violation without filter
+                      int aArg6, int aArg7, int aArg8, int aArg9, int aArg10) {
 
     }
 }
