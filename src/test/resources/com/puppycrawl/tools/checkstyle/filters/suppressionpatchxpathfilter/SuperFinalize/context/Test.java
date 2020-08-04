667de793594ab65f15f401b789d949eb9f50cdc0
HuGanghui <huganghui18@gmail.com>
2020-08-05 00:12:50 +0800

    Issue #234: SuppressionPatchXpathFilter: SuperFinalize's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/SuperFinalize/context/Test.java
@@ -0,0 +1,15 @@
+package TreeWalker.coding.SuperFinalize;
+
+public class Test {
+    public void finalize() throws Throwable {  // violation without filter
+    }
+}
+
+
+
+
+
+class Test1 {
+    public void finalize() throws Throwable {  // violation without filter
+    }
+}
