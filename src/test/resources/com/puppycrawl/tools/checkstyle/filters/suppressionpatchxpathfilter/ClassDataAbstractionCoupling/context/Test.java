18d65efab9ae680dc9e3cc04dca428c15890f002
HuGanghui <huganghui18@gmail.com>
2020-08-05 15:14:42 +0800

    Issue #242: SuppressionPatchXpathFilter: ClassDataAbstractionCoupling's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/ClassDataAbstractionCoupling/context/Test.java
@@ -0,0 +1,13 @@
+package TreeWalker.metrics.ClassDataAbstractionCoupling;
+
+import java.sql.Time;
+import java.util.*;
+
+public class Test {  // violation without filter
+    public void test() {
+        HashSet set = new HashSet();
+        Map map = new HashMap();
+        Date date = new Date();
+        Time time = new Time(2323);
+    }
+}
