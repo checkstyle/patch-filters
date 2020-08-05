ff102b944783632bb07d4025ded9abf938f3e731
HuGanghui <huganghui18@gmail.com>
2020-08-05 16:02:03 +0800

    Issue #248: SuppressionPatchXpathFilter: JavaNCSS's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/JavaNCSS/context/Test.java
@@ -0,0 +1,8 @@
+package TreeWalker.metrics.JavaNCSS;
+
+public class Test {
+    public void test() {  // violation without filter
+        System.out.println();
+        System.out.println();
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/JavaNCSS/newline/Test.java
@@ -0,0 +1,13 @@
+package TreeWalker.metrics.JavaNCSS;
+
+public class Test {
+    public void test() {  // violation without filter
+        System.out.println();
+        System.out.println();
+    }
+
+    public void test1() {  // violation without filter
+        System.out.println();
+        System.out.println();
+    }
+}
