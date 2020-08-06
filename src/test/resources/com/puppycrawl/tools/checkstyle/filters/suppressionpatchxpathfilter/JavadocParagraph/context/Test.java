42c12a3596cc410c784b4b00a316dd3200aa022d
HuGanghui <huganghui18@gmail.com>
2020-08-06 19:39:01 +0800

    Issue #267: SuppressionPatchXpathFilter: JavadocParagraph's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/JavadocParagraph/context/Test.java
@@ -0,0 +1,10 @@
+package TreeWalker.javadoc.JavadocParagraph;
+
+/**
+ * No tag (ok).
+ *
+ * <p>Tag immediately before the text.
+ * <p>No blank line before the tag.  // violation without filter
+ */
+public class Test {
+}
