d7d142bb7849d0259bbf23655ce829d5abf256fe
HuGanghui <huganghui18@gmail.com>
2020-08-06 21:16:10 +0800

    Issue #274: SuppressionPatchXpathFilter: MissingJavadocMethod's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/MissingJavadocMethod/context/Test.java
@@ -0,0 +1,7 @@
+package TreeWalker.javadoc.MissingJavadocMethod;
+
+public class Test {
+
+    public Test() {}  // violation without filter
+
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/MissingJavadocMethod/patchedline/Test.java
@@ -0,0 +1,9 @@
+package TreeWalker.javadoc.MissingJavadocMethod;
+
+public class Test {
+
+    public Test() {}  // violation without filter
+
+    public void test() {}  // violation without filter
+
+}
