03930e859ea4c67128d376675ff34b922d9dd34b
HuGanghui <huganghui18@gmail.com>
2020-08-06 18:46:02 +0800

    Issue #265: SuppressionPatchXpathFilter: InvalidJavadocPosition's context strategy

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionpatchxpathfilter/InvalidJavadocPosition/patchedline/Test.java
@@ -0,0 +1,9 @@
+package TreeWalker.javadoc.InvalidJavadocPosition;
+
+/** violation */  // violation without filter
+/** valid */
+public class Test {
+    /** violation */  // violation without filter
+}
+
+
