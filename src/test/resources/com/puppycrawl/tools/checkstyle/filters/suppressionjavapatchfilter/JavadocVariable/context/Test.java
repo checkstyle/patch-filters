d052cdf35ad1b236dadf422b4a8317933a82ce53
HuGanghui <huganghui18@gmail.com>
2020-08-28 15:52:20 +0800

    Issue #302: add JavadocVariable to CHECK_TO_ANCESTOR_NODES_MAP

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionjavapatchfilter/JavadocVariable/context/Test.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionjavapatchfilter/JavadocVariable/context/Test.java
@@ -3,3 +3,7 @@ package TreeWalker.javadoc.JavadocVariable;
 public class Test {
     private int test;  // violation without filter
 }
+
+class Test1 {
+    private int test;  // violation without filter
+}
