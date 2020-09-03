3429acb0b0c8d548d9dc82906abbee8033a8bb4f
HuGanghui <huganghui18@gmail.com>
2020-09-03 22:46:47 +0800

    Issue #315: Reorganize test resources by check module

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionjavapatchfilter/sizes/MethodLength/patchedline/Test.java
+++
@@ -1,9 +0,0 @@
-package TreeWalker.MethodLength;
-
-public class Test {
-    public void test1() {  // violation without filter
-        System.out.println();
-        System.out.println();
-        System.out.println();
-    }
-}
