dfd859e10223ce920ffb4448b807b6ec4803de2e
rnveach <rveach02@gmail.com>
2022-11-10 21:48:37 -0500

    config: update checkstyle to 10.4

--- src/test/java/com/puppycrawl/tools/checkstyle/AbstractModuleTestSupport.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/AbstractModuleTestSupport.java
@@ -42,8 +42,8 @@ import java.util.stream.Collectors;
 
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.Maps;
+import com.puppycrawl.tools.checkstyle.LocalizedMessage.Utf8Control;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
-import com.puppycrawl.tools.checkstyle.api.Violation;
 import com.puppycrawl.tools.checkstyle.internal.utils.BriefUtLogger;
 import com.puppycrawl.tools.checkstyle.utils.ModuleReflectionUtil;
 
@@ -437,7 +437,7 @@ public abstract class AbstractModuleTestSupport extends AbstractPathTestSupport 
                 messageBundle,
                 Locale.getDefault(),
                 Thread.currentThread().getContextClassLoader(),
-                new Violation.Utf8Control());
+                new Utf8Control());
         final String pattern = resourceBundle.getString(messageKey);
         final MessageFormat formatter = new MessageFormat(pattern, Locale.ROOT);
         return formatter.format(arguments);
